3582b1b0fcc1f009af0f87f18a21b1990924db81
deesync <deesyync@gmail.com>
2026-03-02 11:41:58 +0000

    show results after streaming to compare

--- task/app.py
+++ task/app.py
@@ -53,11 +53,11 @@ async def start(stream: bool) -> None:
 			print("Error:", e)
 			continue
 
-		# # print and store assistant message
-		# print(f"\nAssistant: {assistant_msg.content}")
-		# conv.add_message(assistant_msg)
+		# print and store assistant message
+		print(f"\nAssistant: {assistant_msg.content}")
+		conv.add_message(assistant_msg)
 
 
 asyncio.run(
-	start(True)
+	start(False)
 )
